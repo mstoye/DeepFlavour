f03cd0cb5aaf0cd12a9e474f3958a1eb6a6d8c69
Markus <markus.stoye@cern.ch>
2017-01-26 15:11:07 +0100

    added some PF stuff

--- DeepNtuplizer/plugins/DeepNtuplizer.cc
+++ DeepNtuplizer/plugins/DeepNtuplizer.cc
@@ -4,7 +4,9 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-
+#include <DataFormats/Common/interface/Ptr.h>
+#include "DataFormats/PatCandidates/interface/PackedCandidate.h"
+#include "DataFormats/Candidate/interface/Candidate.h"
 //ROOT includes
 #include "TTree.h"
 #include <TFile.h>
@@ -42,10 +44,23 @@ private:
   TFile *file_ = new TFile("output.root","recreate");
   TTree *tree_ = new TTree("tree","tree");
 
+  // labels (MC truth)
+  float gen_pt_;
+
+  // global event variables
   unsigned int npv_;
 
   // jet variables
   float jet_pt_;
+  float  jet_eta_;
+
+  // PF candidate variables
+  unsigned int n_pfcand_;
+  float  pfcan_pt_[100];
+  float  pfcan_phirel_[100];
+  float  pfcan_etarel_[100];
+  float  pfcan_puppiw_[100];
+
 
 };
 
@@ -57,10 +72,21 @@ DeepNtuplizer::DeepNtuplizer(const edm::ParameterSet& iConfig):
   //now do what ever initialization is needed
   usesResource("TFileService");
 
-  tree_->Branch("npv"    ,&npv_    ,"npv/i"    );
+  tree_->Branch("gen_pt"    ,&gen_pt_    ,"gen_pt_/f"    );
+  // global even variables
+  tree_->Branch("npv"    ,&npv_    ,"npv_/i"    );
 
   // jet variables
   tree_->Branch("jet_pt", &jet_pt_);
+  tree_->Branch("jet_eta", &jet_eta_);
+
+  // PFcanditates per jet
+  tree_->Branch("n_pfcand", &n_pfcand_,"n_pfcand_/i");
+  tree_->Branch("nPF_pt", &pfcan_pt_,"pfcan_pt_[n_pfcand_]/f");
+  tree_->Branch("pfcan_phirel",&pfcan_phirel_,"pfcan_phirel_[n_pfcand_]/f");
+  tree_->Branch("pfcan_etarel",&pfcan_etarel_,"pfcan_etarel_[n_pfcand_]/f");
+  tree_->Branch("pfcan_puppiw",&pfcan_puppiw_,"pfcan_puppiw_[n_pfcand_]/f");
+
 }
 
 
@@ -85,11 +111,36 @@ DeepNtuplizer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
   // clear vectors
   npv_ = vertices->size();
 
+  const static float magic_invalid = -999;
 
   // loop over the jets
   for (const pat::Jet &jet : *jets) {
-    jet_pt_ = jet.pt();
-		tree_->Fill();
+
+    // label: this is what we want to know in data, but only have in MC
+    gen_pt_ = magic_invalid;
+    if(jet.genJet()!=NULL)   gen_pt_ =  jet.genJet()->pt();
+
+
+    // global jet quanties
+      jet_pt_ = jet.pt();
+      jet_eta_ = jet.eta();
+      float etasign = 1.;
+      if (jet.eta()<0) etasign =-1.;
+
+      n_pfcand_ =  jet.numberOfDaughters();
+      for (unsigned int i = 0; i <  jet.numberOfDaughters(); i++)
+	{
+	  const pat::PackedCandidate* PackedCandidate_ = dynamic_cast<const pat::PackedCandidate*>(jet.daughter(i));
+	  pfcan_pt_[i] = PackedCandidate_->pt();
+	  pfcan_phirel_[i] = reco::deltaPhi(PackedCandidate_->phi(),jet.phi());
+	  pfcan_etarel_[i] = etasign*(PackedCandidate_->eta()-jet.eta());
+	  pfcan_puppiw_[i] = PackedCandidate_->puppiWeight();	  
+	}  
+ 
+
+
+
+      tree_->Fill();
   }
 }
 
